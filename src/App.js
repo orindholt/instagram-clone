f6c5c6bd3e6c5456bf8e336c1afaa995f03bda0f
orindholt <83012141+orindholt@users.noreply.github.com>
2022-01-10 13:52:40 +0100

    Add theme to project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import "./reset.css";
+import { theme } from "./Components/Theme";
+const { colors: {red} } = theme;
+
+const style = css`
+  color: ${red}
+`;
+
 const App = () => {
   return (
     <div className="App">
-      
+      <p css={style}>Test</p>
     </div>
   );
 }
---
+++ src/Components/Theme.js
@@ -0,0 +1,19 @@
+const obj = {
+  colors: {
+    red: "#ED4944",
+    blue: "#0095f6",
+    lightBlue: "#B4E0FC",
+    gray: "#8e8e8e",
+    lightGray: "#DBDBDB",
+    black: "#262626"
+  },
+  sizes: {
+    big: "30px",
+    semi: "18px",
+    medium: "16px",
+    small: "14px",
+    smaller: "12px"
+  },
+}
+
+export const theme = {...obj};
