8cb0e14d4cb77190a778ac1b62b6d953e8c43969
MarcusHjorth <U773559@rts-365.dk>
2022-01-13 08:50:24 +0100

    Test

---
+++ src/Components/Theme.js
@@ -0,0 +1,20 @@
+const obj = {
+    colors: {
+        red: "#ED4944",
+        blue: "#0095F6",
+        lightBlue: "#B4E0FC",
+        gary: "#8e8e8e",
+        lightGray: "#DBDBDB",
+        black: "#262626"
+    },
+    sizes: {
+        big: "30px",
+        large: "22px",
+        semi: "18px",
+        medium: "16px",
+        small: "14px",
+        smaller: "12px"
+    }
+}
+ 
+export default obj;
