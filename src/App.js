e1f43383aacf2a274f97d4292eb332785b6dfe15
MarcusHjorth <U773559@rts-365.dk>
2022-01-13 08:51:07 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+import Theme from "./Components/Theme";
+import Header from "./Templates/Header";
+
+<script src="https://kit.fontawesome.com/ccda818105.js" crossorigin="anonymous"></script>
+
+const { red } = Theme.colors
+
+const style = css`
+  color: ${red}
+`;
+
+
 const App = () => {
   return (
     <div className="App">
-      
+      <Header />
+      <p css={style}>Hello, this is a test</p>
     </div>
   );
 }
