d01ef3eac66d7b8d2697f193baa61f768a3cb81d
SebastianCCC <u2407190@rts-365.dk>
2022-01-14 09:07:46 +0100

    change component pos

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,14 @@
-import Post from "../Templates/Post";
-import UserProposal from './Components/UserProposal'
+import Post from '../Templates/Post'
+import UserProposal from '../Components/UserProposal'
 
 const Home = () => {
   // Add Templates here, for home page
   return (
     <>
-    <Post />
-    <UserProposal />
+      <Post />
+      <UserProposal />
     </>
-  );
+  )
 }
 
 export default Home
