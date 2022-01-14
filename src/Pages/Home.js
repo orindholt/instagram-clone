fb5377cdd419c26a65b6244cc2f2053afbfe645c
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-01-14 08:40:39 +0100

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,13 @@
 import Post from "../Templates/Post";
+import UserProposal from './Components/UserProposal'
 
 const Home = () => {
   // Add Templates here, for home page
   return (
+    <>
     <Post />
+    <UserProposal />
+    </>
   );
 }
 
