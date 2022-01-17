b24da11d723348b19ebfb4530143949c96455c2e
SebastianCCC <u2407190@rts-365.dk>
2022-01-17 14:36:13 +0100

    make validation on site and cleanup

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,23 +1,26 @@
 /** @jsxImportSource @emotion/react */
-import { colors, sizes } from "./Theme";
-import {css} from "@emotion/react";
-
+import { colors, sizes } from './Theme'
+import { css } from '@emotion/react'
 
 const SearchBar = () => {
-    return ( 
-        <form className="" action="">
-            <input css={ css`
-            height: 25px;
-            width: 236px;
+  return (
+    <form className="">
+      <input
+        css={css`
+          height: 25px;
+          width: 236px;
 
-            background-color: ${colors.lightGray};
-            border-radius: 5px;
-            padding-left: 16px;
-            padding-right: 16px;
-            `}
-             type="text" placeholder="Søg" name="search" />
-        </form>
-     );
+          background-color: ${colors.lightGray};
+          border-radius: 5px;
+          padding-left: 16px;
+          padding-right: 16px;
+        `}
+        type="text"
+        placeholder="Søg"
+        name="search"
+      />
+    </form>
+  )
 }
- 
-export default SearchBar;
+
+export default SearchBar
