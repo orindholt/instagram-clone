a675c55bdea1297f66137eacd8955079f5d59f30
orindholt <83012141+orindholt@users.noreply.github.com>
2022-01-17 13:20:58 +0100

    Add header to home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import Post from "../Templates/Post";
+import Header from "../Templates/Header"
 
 const Home = () => {
 	// Add Templates here, for home page
