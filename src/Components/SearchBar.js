6448b3b9e4373f61c4313176f718833a4c2f9d36
orindholt <83012141+orindholt@users.noreply.github.com>
2022-01-18 09:59:11 +0100

    Add functionality to searchbar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,21 +1,39 @@
 /** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import { useState } from "react";
+import SearchIcon from "./SearchIcon";
 import { colors, sizes } from "./Theme";
-import {css} from "@emotion/react";
-
 
 const SearchBar = () => {
+    const [ inFocus, setInFocus ] = useState(false);
+    const [ value, setValue ] = useState("");
     return ( 
-        <form className="" action="">
-            <input css={ css`
-            height: 25px;
-            width: 236px;
+        <form css={ css`
+            height: 34px;
+            width: 209px;
 
             background-color: ${colors.lightGray};
-            border-radius: 5px;
-            padding-left: 16px;
-            padding-right: 16px;
+            border-radius: 7px;
+            padding: 0 16px;
+            display: flex;
+            align-items: center;
+            gap: 10px;
+        `}>
+            {!inFocus && <SearchIcon />}
+            <input 
+                value={value}
+                onChange={(e)=>{setValue(e.target.value)}}
+                css={css`
+                    background-color: ${colors.lightGray};
+                    font-size: ${sizes.small};
+                    &::placeholder {
+                        font-size: ${sizes.medium};
+                        font-weight: 300;
+                    }
             `}
-             type="text" placeholder="Søg" name="search" />
+            onFocus={()=>{setInFocus(true)}}
+            onBlur={()=>{setInFocus(false)}}
+            type="text" placeholder="Søg" name="search" id="search" />
         </form>
      );
 }
