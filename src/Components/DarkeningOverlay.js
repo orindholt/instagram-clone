0578caee9b9b1aa7e145b2b989b437ee4d5b632d
orindholt <83012141+orindholt@users.noreply.github.com>
2022-01-25 12:29:05 +0100

    Fix darkening overlay

--- src/Components/DarkeningOverlay.js
+++ src/Components/DarkeningOverlay.js
@@ -1,8 +1,7 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
-import React, { useState, useEffect } from 'react';
 
-const DarkeningOverlay = (props,{ setPostSettings }) => {
+const DarkeningOverlay = ({ setPostSettings, children }) => {
 
   function handleOnClick(e){
     console.log('target:');
@@ -10,28 +9,25 @@ const DarkeningOverlay = (props,{ setPostSettings }) => {
     console.log('currentTarget:');
     console.log(e.currentTarget);
     if(e.target === e.currentTarget){
-      props.setPostSettings(false)
+      setPostSettings(false)
     }
   }
-
-    console.log(props);
     return ( 
         <div className='stopScroll'
           onClick={handleOnClick}
           css={css`
+            position: fixed;
+            top: 0;
+            left: 0;
             z-index: 100;
             display: flex;
             justify-content: space-around;
             align-items: center;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            width: 100vw;
-            height: 100vh;
+            width: 100%;
+            height: 100%;
             background-color: rgba(0, 0, 0, 65%);
          `}>
-         {props.children}
+         {children}
       </div>
      );
 }
