f72cfd4aa45dc9ab5fd7f164eb823b43ec539503
SebastianCCC <u2407190@rts-365.dk>
2022-01-25 14:19:02 +0100

    change profileIcon

--- src/Components/ProfileIcon.js
+++ src/Components/ProfileIcon.js
@@ -1,11 +1,12 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react'
 import { colors } from '../Components/Theme'
+import userImg from '../assets/userimg.jpg'
 
 const ProfileIcon = ({ size = '32px' }) => {
   return (
     <img
-      src="https://thumbs.dreamstime.com/b/default-avatar-profile-vector-user-profile-default-avatar-profile-vector-user-profile-profile-179376714.jpg"
+      src={userImg}
       alt="Profile"
       css={css`
         width: ${size};
